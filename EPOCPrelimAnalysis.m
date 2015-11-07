2de2f9fed690ac971499abf31bf34c167cb299ed
mdt <mdtdev@users.noreply.github.com>
2015-11-07 16:18:04 -0500

    Finished v. 1.0 EPOCPrelimAnalysis.m
    First pass at a Navin-style analysis script for the new data files.
    Works in testing, still needs to be reduced into a function and need to
    re-locate the files, I think.

--- EPOCPrelimAnalysis.m
+++ EPOCPrelimAnalysis.m
@@ -27,5 +27,58 @@ fprintf('(3) Run this from the data directory, unless you know what you\n\tare d
 
 % Grab the time series with the getter function
 
-EEG                  = pop_loadset('filename',datasetFileName);
-[cbt, nc, cNames,sr] = ge_extractTimeCourses(EEG);
+EEG                   = pop_loadset('filename',datasetFileName);
+[cbt, nc, cNames, sr] = ge_extractTimeCourses(EEG);
+
+% Generate the plots for each channel:
+
+for ii = 1:nc
+
+  % Setup for a channel
+  channelName = cNames(ii,:);
+  figureFile  = [datasetFileRoot '_prelimfigure' '_' channelName '.fig'];
+  fprintf(['\tChannel: ' channelName ' to file: ' figureFile '\n']);
+
+  % Overall figure
+  figure;
+
+  % Time Domain Plot
+  t     = linspace(0, length(cbt(ii,:))/sr, length(cbt(ii,:)));
+  tempe = cbt(ii,:) - mean(cbt(ii,:));
+
+  subplot(2,2,1);
+  plot(t,tempe);
+  hold on;
+  plot(xlim, [0 0], '-r');
+  hold off;
+  xlabel('Time in seconds');
+  ylabel('Amplitude');
+  title(['Mean removed time domain plot for channel: ' channelName]);
+
+  % Fourier Spectrum Plot
+  L    = length(tempe);
+  NFFT = 2^nextpow2(L);
+  Y    = fft(tempe, NFFT)/L;
+  f    = (sr/2) * linspace(0, 1, (NFFT/2) + 1);
+
+  subplot(2,2,2);
+  plot(f, 2*abs(Y(1:(NFFT/2)+1)));
+  title(['Single-Sided Amplitude Spectrum using FFT for channel: ' channelName]);
+  xlabel('Frequency (Hz)');
+  ylabel('|Y(f)|');
+
+  % Spectrogram Plot
+  [S, Freq, T, P] = spectrogram(tempe, sr, 32, sr, sr);
+
+  subplot(2,2,[3 4]);
+  surf(T, Freq, 10*log10(P), 'edgecolor', 'none');
+  axis tight;
+  view(0,90);
+  title(['Spectrogram: Entire spectrum for whole session, channel: ' channelName]);
+  colorbar;
+  xlabel('Time (Seconds)'); ylabel('Hz');
+
+  % Save the figure
+  savefig(gcf, figureFile);
+  close all;
+end
--- asil.m
+++ asil.m
@@ -4,18 +4,60 @@
 
 ii = 1;  % This will be the index for the loop
 
-channelName = cNames(ii,:);
-figureFile  = [datasetFileRoot '_prelimfigure' '_' channelName '.pdf'];
+% Setup for a channel
 
-% time domain plot
+channelName = cNames(ii,:);
+figureFile  = [datasetFileRoot '_prelimfigure' '_' channelName '.fig'];
+
+fprintf(['\tChannel: ' channelName ' to file: ' figureFile '\n']);
+
+% Overall figure
+
+figure;
+
+% Time Domain Plot
+%
 
 t     = linspace(0, length(cbt(ii,:))/sr, length(cbt(ii,:)));
 tempe = cbt(ii,:) - mean(cbt(ii,:));
 
-figure;
+subplot(2,2,1);
 plot(t,tempe);
+hold on;
+plot(xlim, [0 0], '-r');
+hold off;
 xlabel('Time in seconds');
 ylabel('Amplitude');
-title('Mean Removed Time domain plot');
+title(['Mean removed time domain plot for channel: ' channelName]);
 
-saveas(gcf, figureFile, 'pdf');
+% Fourier Spectrum Plot
+%
+
+L    = length(tempe);
+NFFT = 2^nextpow2(L);
+Y    = fft(tempe, NFFT)/L;
+f    = (sr/2) * linspace(0, 1, (NFFT/2) + 1);
+
+subplot(2,2,2);
+plot(f, 2*abs(Y(1:(NFFT/2)+1)));
+title(['Single-Sided Amplitude Spectrum using FFT for channel: ' channelName]);
+xlabel('Frequency (Hz)');
+ylabel('|Y(f)|');
+
+% Spectrogram Plot
+%
+
+[S, Freq, T, P] = spectrogram(tempe, sr, 32, sr, sr);
+
+subplot(2,2,[3 4]);
+surf(T, Freq, 10*log10(P), 'edgecolor', 'none');
+axis tight;
+view(0,90);
+title(['Spectrogram: Entire spectrum for whole session, channel: ' channelName]);
+colorbar;
+xlabel('Time (Seconds)'); ylabel('Hz');
+
+% Save the figure
+savefig(gcf, figureFile);
+
+close all;
