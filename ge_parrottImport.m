1219b34c4ad52df90911bd05b9f829c842dc94b2
Matthew Turner <mdtdev@users.noreply.github.com>
2016-06-17 11:38:33 -0400

    Edits for set/edf files

--- ge_parrottImport.m
+++ ge_parrottImport.m
@@ -8,8 +8,16 @@ function AIS = ge_parrottImport(filename)
     %    that contains the EEG data only.  The original data with markers
     %    is in EEG2; the selected/filtered data is in EEG_only.
 
+    if regexp(filename,'set$')
+        EEG2 = pop_loadset(filename);
+    elseif regexp(filename,'edf$')
+        EEG2 = pop_biosig(filename);
+    else
+        error('ge_handContraction: File type unknown');
+    end
+    
     %EEG2     = pop_biosig(filename);
-    EEG2     = pop_loadset(filename);
+    %EEG2     = pop_loadset(filename);
     EEG_only = pop_select(EEG2, 'channel', eegChannels);
     EEG_only = pop_eegfilt(EEG_only, lowerBound, upperBound, [], [0], 0, 0, 'fir1', 0);
 
